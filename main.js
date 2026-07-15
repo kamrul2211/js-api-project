163eb6740143db0261283c6449244f148ec79299
Md Kamrul Hassan <146529748+kamrul2211@users.noreply.github.com>
2026-07-15 12:44:33 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+fetch('https://jsonplaceholder.typicode.com/users')
+  .then(response => response.json())
+  .then(data => getdata.innerHTML = data.map((item, i) => {
+    return (`
+    <tr>
+    <td>${item.name}</td>
+    <td>${item.email}</td>
+    <td>${item.website}</td>
+    <td>
+    <button class="btn btn-primary btn-sm">Edit</button>
+    <button class="btn btn-danger"> Delete</button>
+    </td>
+    
+    </tr>
+    `)
+
+  }).join('')
+  )
+let getdata = document.getElementById('user-content')
